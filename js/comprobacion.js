f44f2712c3e1e91d622b0cfd36864f54c82084b3
Jorge Alonso <alu0100767803@ull.edu.es>
2016-11-09 11:32:58 +0000

    javascript funcionando

--- js/comprobacion.js
+++ js/comprobacion.js
@@ -1,16 +1,12 @@
-function validar() {
+function validar(email) {
 
-	emailUsuario = getElementByID("email");
-	error = true;
 	expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	error = true;
 
-	if(emailUsuario == null){
-		alert("Error");
-	}
-    if (!expr.test(emailUsuario)){
+    if (!expr.test(email.value)){
+    	alert("Error: La dirección de correo " + email.value + " es incorrecta.");
     	error = false;
-    	alert("Error: La dirección de correo " + emailUsuario + " es incorrecta.");
     }
-     
-	return error;
+
+	return error;    
 }
